b6eff4609e23917dd8c47b354913c3c67f3d3d60
lmcglynn <liamo.mcglynn@comcast.net>
2025-03-18 02:21:51 -0700

    most of it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Bracket from './Components/Bracket';
+import ToggleButton from './Components/ToggleButton';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <ToggleButton />
+          <Bracket />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BracketProvider } from './BracketProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BracketProvider>
+      <App />
+    </BracketProvider>
   </React.StrictMode>
 );
 
